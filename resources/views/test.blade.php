ddd262ca2381a349d721a7f85c4a0e22ec56cf60
NinFrei <n.freiman@yahoo.com>
2022-06-27 12:30:15 +0200

    change

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('/js/app.js') }}"></script>
+</head>
+<body>
+   <h1>Die Sohne scheint</h1> 
+</body>
+</html>
