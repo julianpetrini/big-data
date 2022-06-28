65bb458f7b75629c4f4e0699e8557b24bfdda7c1
julianpetrini <bwmisojos@gmail.com>
2022-06-28 11:14:55 +0200

    updated card style and footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="nav_color text-center text-white footerMtop">
+            <a class="text-white" href="https://mdbootstrap.com/">BIG DATA | LET'S TALK</a>
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+</footer>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,9 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-   
     <script src="{{ asset('/js/app.js') }}"></script>
+    {{-- THIS IS FOR THE ICONS --}}
+    <script src="https://kit.fontawesome.com/f30a166a26.js" crossorigin="anonymous"></script>
     
     
 
