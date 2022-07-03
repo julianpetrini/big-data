9da5d89a53e3e1df96172ea23cf30824042efe67
ruslan70 <ruslan.khatuev70@gmail.com>
2022-07-04 00:14:18 +0200

    add  increments by id

--- database/migrations/2022_06_28_074817_create_posts_table.php
+++ database/migrations/2022_06_28_074817_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('author');
             $table->string('title');
             $table->text('content');
