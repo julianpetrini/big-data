8b0533af6c37375af27a0b88dff1b847b06751fe
NinFrei <n.freiman@yahoo.com>
2022-07-06 13:54:28 +0200

    latest change and merge

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -1,13 +1,27 @@
 @extends ('layouts/master')
 
-@section ('aboutus')
+@section('aboutus')
+    <div class="postDetailsBox">
+        <img src="{{ asset('img/nav_logo.png') }}" class="img-fluid rounded mx-auto d-block m-2" alt="Responsive image">
 
-<div class="postDetailsBox">
-    <img src="{{ asset('img/nav_logo.png') }}" class="img-fluid rounded mx-auto d-block m-2" alt="Responsive image">
+        <h2 class="title_header"><strong> About us </strong></h2>
+        <h3 class="wir"><strong><i><ins>Ruslan, Beni, Sven, Nina, Ajoub, Julian</ins></i></strong></h3>
+        <p class="pMain">Wir sind die kleine Gruppe von Menschen die grosse Ideen haben.<br>
+            Wir wollen gemeinsam unseren
+            Gedanken in erstklassige digitale Lösungen verwandeln.
+        </p>
+        <h3 class="wir_1">Unsere Teammitglieder beschreiben sich als : </h3>
+        <div class="container-fluid mt-3">
+            <div class="row">
 
-    <h2 class="title_header"><strong> about us </strong></h2>
-    <p class="pMain">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti ex esse dolorum qui est impedit beatae recusandae voluptates eos ipsum? Sint incidunt magnam suscipit earum saepe? Nisi quibusdam velit doloremque!</p>
-</div>
-
-
+                <div class="col p-3 text-white"><ins>1. SELBSTBEWUSST</ins>
+                    Wir treten als selbstbewusste und seriöse Experten auf, um unseren Kunden Rückhalt zu geben.</div>
+                <div class="col p-3 text-white">2. WERTSCHÄTZEND <br>
+                    Wir wertschätzen die Geschäftsbeziehungen zu unseren Kunden, weil unser Erfolg davon abhängt.</div>
+                <div class="col p-3 text-white">
+                    3. INTELLIGENT Wir sind in der Lage komplexe Probleme zu verstehen und die richtigen Lösungen zu
+                    finden.></div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,43 +2,42 @@
 
 
 @section('header')
-<header>
+    <header>
 
-    <img src="{{ asset('img/logo_big_data_white.png') }}" class="img-fluid rounded mx-auto d-block"
-        alt="Responsive image">
+        <img src="{{ asset('img/logo_big_data_white.png') }}" class="img-fluid rounded mx-auto d-block"
+            alt="Responsive image">
 
-    <h1 class="title_header">Ihre besten Daten finden und kommentieren</h1>
+        <h1 class="title_header">Ihre besten Daten finden und kommentieren</h1>
 
-    <p class="pMain">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur cumque blanditiis autem.
-        Laboriosam temporibus veniam rerum labore commodi totam ad nisi fuga, rem alias veritatis iure facilis sapiente
-        quam
-        ipsam!</p>
+        <p class="pMain"><em>
+                Der Begriff „Big Data“ bezeichnet die große Menge an strukturierten und unstrukturierten Daten, die
+                Unternehmen Tag für Tag überschwemmen. Nicht die Daten selbst sind so wichtig. Was zählt, ist das, was
+                Unternehmen mit den Daten machen. Große Datenmengen können analysiert werden, um Erkenntnisse zu gewinnen
+                und
+                auf deren Grundlage bessere Entscheidungen zu treffen und das Unternehmen strategisch auszurichten.</em>
+        </p>
 
-</header>
+    </header>
 @endsection
 
 {{-- THIS WOULD BE THE FORMAT OF THE POST --}}
-@section ('posts')
+@section('posts')
+    <section class="post_bbox text-white">
+        @foreach ($posts as $post)
+            <div class="card cardBgColor cardBoxStyle">
 
-
-<section class="post_bbox text-white">
-    @foreach ($posts as $post)
-    <div class="card cardBgColor cardBoxStyle">
-       
-        <img class="card-img-top" src="data:image/jpg;base64,{{ chunk_split(base64_encode($post->postPic)) }}" alt="Card image cap">
-        <div class="card-body cardedit">
-            <h5 class="card-title "><strong>{{ $post->title }}</strong></h5>
-            <div class="aYd">
-                <h6 class="card-subtitle mb-2 text-white">{{ $post->author }}</h6>
-                <h6 class="card-subtitle mb-2 fw-lighter">{{ $post->created_at }}</h6>
+                <img class="card-img-top" src="data:image/jpg;base64,{{ chunk_split(base64_encode($post->postPic)) }}"
+                    alt="Card image cap">
+                <div class="card-body cardedit">
+                    <h5 class="card-title "><strong>{{ $post->title }}</strong></h5>
+                    <div class="aYd">
+                        <h6 class="card-subtitle mb-2 text-white">{{ $post->author }}</h6>
+                        <h6 class="card-subtitle mb-2 fw-lighter">{{ $post->created_at }}</h6>
+                    </div>
+                    <p class="card-text cTextElip">{{ $post->content }}</p>
+                    <a href="/postdetail/{{ $post->id }}" class="btn buttonCustom text-white">Full post</a>
+                </div>
             </div>
-            <p class="card-text cTextElip">{{ $post->content }}</p>
-            <a href="/postdetail/{{ $post->id }}" class="btn buttonCustom text-white">Full post</a>
-        </div>
-    </div>
-    @endforeach
-</section>
-
+        @endforeach
+    </section>
 @endsection
-
-
