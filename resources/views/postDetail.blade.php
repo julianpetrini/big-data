e8d282da8f4d49aaa615b60286592e684eb555d7
ruslan70 <ruslan.khatuev70@gmail.com>
2022-07-06 16:06:55 +0200

    ruslan delete duplicate

--- resources/views/postDetail.blade.php
+++ resources/views/postDetail.blade.php
@@ -7,52 +7,33 @@
         <img src="data:image/jpg;base64,{{ chunk_split(base64_encode($post->postPic)) }}"
             class="img-fluid rounded mx-auto d-block m-2" alt="Responsive image">
 
-    <h2 class="title_header"><strong>{{ $post->title }}</strong></h2>
-    <h3 class="title_header">{{ $post->author }}</h3>
-    <p class="pMain">{{ $post->content }}</p>
-</div>
-@auth
-<div class="postDetailsBox">
-    <h3 class="title_header"><strong> LEAVE YOUR COMMENTS </strong></h3>
-    <form action="/postDetail" method="post">
-            <!-- hidden field holding message->id to remember, which message
-            the new comment will belong to -->
-            <input type="hidden" name="post_id" value="{{ $post->id }}">
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label text-white">Name</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="{{Auth::user()->name}}" readonly>
+        <h2 class="title_header"><strong>{{ $post->title }}</strong></h2>
+        <h3 class="title_header">{{ $post->author }}</h3>
+        <p class="pMain">{{ $post->content }}</p>
+    </div>
+    @auth
+        <div class="postDetailsBox">
+            <h3 class="title_header"><strong> LEAVE YOUR COMMENTS </strong></h3>
+            <form action="/postDetail" method="post">
+                    <!-- hidden field holding message->id to remember, which message
+                    the new comment will belong to -->
+                    <input type="hidden" name="post_id" value="{{ $post->id }}">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label text-white">Name</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="{{Auth::user()->name}}" readonly>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label text-white">Tell us what you think</label>
+                    <textarea  name="comment" class="form-control" id="exampleFormControlTextarea1" rows="5" 
+                    placeholder="Write an amazing comment"></textarea>
+                   
+                </div>
+                @csrf
+                <button type="submit" class="btn buttonCustom text-white">Submit</button>
+            </form> 
         </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label text-white">Tell us what you think</label>
-            <textarea  name="comment" class="form-control" id="exampleFormControlTextarea1" rows="5" 
-            placeholder="Write an amazing comment"></textarea>
-            
-        </div>
-        @csrf
-        <button type="submit" class="btn buttonCustom text-white">Submit</button>
+    
 
-    <h2 class="title_header"><strong>{{ $post->title }}</strong></h2>
-    <h3 class="title_header">{{ $post->author }}</h3>
-    <p class="pMain">{{ $post->content }}</p>
-</div>
-@auth
-<div class="postDetailsBox">
-    <h3 class="title_header"><strong> LEAVE YOUR COMMENTS </strong></h3>
-    <form action="/postDetail" method="post">
-            <!-- hidden field holding message->id to remember, which message
-            the new comment will belong to -->
-            <input type="hidden" name="post_id" value="{{$post->id}}">
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label text-white">Name</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="{{Auth::user()->name}}" readonly>
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label text-white">Tell us what you think</label>
-            <textarea  name="comment" class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Write an amazing comment"></textarea>
-
-    </form>
-
-        </div>
     @endauth
     </section>
 
@@ -61,32 +42,11 @@
     <section class="postDetailsBox">
         <!-- loop through the comment list of a message and display the comment text and user -->
         @foreach ($post->comments as $comment)
-<<<<<<< HEAD
-            <ul class="ul_comments text-white">           
-=======
             <ul class="ul_comments text-white">
             
->>>>>>> 629d6bff9e0190168e8d5a731a4a49058c20599f
                 <li class="liComment">{{$comment->comment}}</li>
                 <li class="liComment">{{$comment->author}}</li>
             </ul>
-<<<<<<< HEAD
-        @auth     
-                <form action="/comment/{{$comment->id}}" method="post">
-                    @csrf
-                    @method('delete')
-                    {{-- <button class="btn buttonCustom text-white" type="submit">Edit</button> --}}
-                    <a href="editComment" class="btn buttonCustom text-white">Edit</a>
-                    <button onclick="return confirm('Do You Really Want to Delete The post')"
-                        class="btn buttonCustom text-white">Delete</button>                  
-
-                    
-                </form>
-            
-
-        @endauth
-    @endforeach
-=======
         @auth                
                 <form action="/comment/{{$comment->id}}" method="post">
 
@@ -101,7 +61,6 @@
         @endauth
 
         @endforeach
->>>>>>> 629d6bff9e0190168e8d5a731a4a49058c20599f
 
     </section>
 @endsection
